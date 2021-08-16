f14b7cb790aca7c05244ba9ccdf064dae211e054
Saket Verma <saket3324verma@gmail.com>
2021-08-16 19:10:45 +0530

    Create new account

--- src/App.js
+++ src/App.js
@@ -1,13 +1,185 @@
-import './App.css';
-import React from "react";
-import Login from "./Components/Login";
-
+import "./App.css";
+import "./Components/CreateAccount.css";
+import "./Components/Login.css";
+import React, { useState } from "react";
+import axios from "axios";
+import Homepage from "./Components/Homepage";
 function App() {
-  let isLogin = false;
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isCreateAccount, setIsCreateAccount] = useState(false);
+
+  const [userDataName, setUserDataName] = useState("");
+  const [userDataUserName, setUserDataUserName] = useState("");
+  const [userDataEmail, setUserDataEmail] = useState("");
+  const [userDataPassword, setUserDataPassword] = useState("");
+
+  let id = 0;
+
+  function submitLogin() {
+    console.log({ user: user, pass: password });
+
+    axios
+      .post("http://localhost:8000/api/login", {
+        user: { username: user, password: password },
+      })
+      .then((response) => {
+        console.log("Login Response", response);
+        id = response.data.id;
+        if (response.status === 200) setIsLoggedIn(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoggedIn(false);
+      });
+  }
+
+  function toggleCreateAccount() {
+    setIsCreateAccount(!isCreateAccount);
+    setUserDataName("");
+    setUserDataUserName("");
+    setUserDataEmail("");
+    setUserDataPassword("");
+  }
+
+  function submitCreateAccount() {
+    console.log({
+      name: userDataName,
+      userName: userDataUserName,
+      email: userDataEmail,
+      password: userDataPassword,
+    });
+
+    let email = document.getElementById("userEmail");
+    let isValid = false;
+
+    if (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(userDataEmail)) {
+      isValid = true;
+      email.classList.remove("invalid");
+    } else {
+      isValid = false;
+      email.classList.add("invalid");
+    }
+
+    if (isValid) {
+      axios
+        .post("http://localhost:8000/api/addUser/", {
+          user: {
+            name: userDataName,
+            username: userDataUserName,
+            email: userDataEmail,
+            password: userDataPassword,
+            isOnline: 1,
+            lastLoggedOut: null,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
   return (
     <div className="App">
-      {!isLogin && <Login />}
-    </div>  
+      {!isLoggedIn && !isCreateAccount && (
+        <div className="login-container">
+          <div className="input-container">
+            <legend className="legend">Username</legend>
+            <input
+              type="text"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              className="input"
+            ></input>
+          </div>
+
+          <div className="input-container">
+            <legend className="legend">Password</legend>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input"
+            ></input>
+          </div>
+
+          <div className="button-container">
+            <button className="btn-primary" onClick={() => submitLogin()}>
+              Login
+            </button>
+            <button
+              className="btn-primary"
+              onClick={() => toggleCreateAccount()}
+            >
+              Create Account
+            </button>
+          </div>
+        </div>
+      )}
+      {!isLoggedIn && isCreateAccount && (
+        <div className="create-account-container">
+          <div className="title">Create Account</div>
+          <div className="input-container">
+            <legend className="legend">Name</legend>
+            <input
+              type="text"
+              value={userDataName}
+              onChange={(e) => setUserDataName(e.target.value)}
+              className="input"
+            ></input>
+          </div>
+          <div className="input-container">
+            <legend className="legend">Username</legend>
+            <input
+              type="text"
+              value={userDataUserName}
+              onChange={(e) => setUserDataUserName(e.target.value)}
+              className="input"
+            ></input>
+          </div>
+          <div className="input-container">
+            <legend className="legend">Email</legend>
+            <input
+              id="userEmail"
+              type="email"
+              value={userDataEmail}
+              onChange={(e) => setUserDataEmail(e.target.value)}
+              className="input"
+            ></input>
+          </div>
+          <div className="input-container">
+            <legend className="legend">Password</legend>
+            <input
+              type="password"
+              value={userDataPassword}
+              onChange={(e) => setUserDataPassword(e.target.value)}
+              className="input"
+            ></input>
+          </div>
+          <div className="button-container">
+            <button
+              className="btn-primary"
+              onClick={() => {
+                submitCreateAccount();
+              }}
+            >
+              Create Account
+            </button>
+            <button
+              className="btn-primary"
+              onClick={() => toggleCreateAccount()}
+            >
+              Back
+            </button>
+          </div>
+        </div>
+      )}
+      {isLoggedIn && !isCreateAccount && <Homepage id={id} />}
+    </div>
   );
 }
 
---
+++ src/Components/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Homepage(props) {
+
+    // const id = props.id;
+
+    return(
+        <div className="home-container">
+            Welcome
+        </div>
+    )
+}
+
+export default Homepage;
